8c09131dbde36225ce7182a7d566f4094f5c81d0
suyeon <skan818@aucklanduni.ac.nz>
2020-09-09 01:04:29 +0000

    Added training for mask rcnn

---
+++ embryo/train_mask_4-008.py
@@ -0,0 +1,9 @@
+#batch size 4
+#lr 0.008
+import os
+os.chdir("/data/embryo/models/research")
+
+os.system("python object_detection/model_main_tf2.py \
+--pipeline_config_path /data/embryo/models/research/model.config \
+--model_dir /data/embryo/mask_rcnn/data/model-4-008 \
+--alsologtostderr")

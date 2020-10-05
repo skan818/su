dfd97dd67326ceebcb614ae56606fecc82741260
suyeon <skan818@aucklanduni.ac.nz>
2020-10-05 02:04:34 +0000

    Added yolo evaluations

---
+++ embryo/analyse_2540_hp.py
@@ -0,0 +1,10 @@
+import os
+os.chdir("/data/embryo/tfrecords/yolov3-tf2")
+
+os.system("python size_over_time.py \
+--classes /data/embryo/random/classes.txt \
+--weights /data/embryo/tfrecords/yolov3-tf2/checkpoints/2540_hp.tf \
+--useMish True \
+--tiny True \
+--num_classes 2 \
+--output size_over_time_2540_hp")
